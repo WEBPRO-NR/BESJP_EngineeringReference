ff83eecb9db5dab83f3b35b4d3ff5b8362df130c
Yusuke Arai <arai@youworks.co.jp>
2020-06-08 16:48:20 +0900

    CSVからXMLへの変換仕様書を統合

--- convert_adoc_to_html.sh
+++ convert_adoc_to_html.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname "$0")
+
+# 計算仕様書の変換
 for ADOC in $(find "$DIR" -name '*.adoc' -maxdepth 1 -printf '%f\n'); do
     INPUT_ADOC="$ADOC"
     OUTPUT_HTML=docs/${ADOC/.adoc/.html}
     echo "Convert '$INPUT_ADOC' to '$OUTPUT_HTML'."
     asciidoctor -o "$DIR"/"$OUTPUT_HTML" "$DIR"/"$INPUT_ADOC"
 done
+
+# CSVからXMLへの変換仕様書の変換
+INPUT_ADOC=csv2xml/index.adoc
+OUTPUT_HTML=docs/csv2xml/index.html
+echo "Convert '$INPUT_ADOC' to '$OUTPUT_HTML'."
+asciidoctor -o "$DIR"/"$OUTPUT_HTML" "$DIR"/"$INPUT_ADOC"

3f5e683d6df94549c029b2ae6ac17a75f72af167
MasatoMiyata <myt.masato@gmail.com>
2020-12-25 14:36:02 +0900

    誤字修正

--- convert_adoc_to_html.sh
+++ convert_adoc_to_html.sh
@@ -10,7 +10,7 @@ for ADOC in $(find "$DIR" -name '*.adoc' -maxdepth 1 -printf '%f\n'); do
     asciidoctor -o "$DIR"/"$OUTPUT_HTML" "$DIR"/"$INPUT_ADOC"
 done
 
-# 計算仕様書の変換（公開用）
+# 計算仕様書の変換（開発者用）
 for ADOC in $(find "$DIR" -name '*.adoc' -maxdepth 1 -printf '%f\n'); do
     INPUT_ADOC="$ADOC"
     OUTPUT_HTML=docs/dev/${ADOC/.adoc/.html}

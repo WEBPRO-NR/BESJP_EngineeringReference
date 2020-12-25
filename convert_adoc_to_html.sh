1260975e30acc69b91a2e39de598ffdf6410da0f
MasatoMiyata <myt.masato@gmail.com>
2020-12-25 14:34:40 +0900

    開発者用html生成機能の追加

--- convert_adoc_to_html.sh
+++ convert_adoc_to_html.sh
@@ -2,12 +2,20 @@
 
 DIR=$(dirname "$0")
 
-# 計算仕様書の変換
+# 計算仕様書の変換（公開用）
 for ADOC in $(find "$DIR" -name '*.adoc' -maxdepth 1 -printf '%f\n'); do
     INPUT_ADOC="$ADOC"
     OUTPUT_HTML=docs/${ADOC/.adoc/.html}
     echo "Convert '$INPUT_ADOC' to '$OUTPUT_HTML'."
     asciidoctor -o "$DIR"/"$OUTPUT_HTML" "$DIR"/"$INPUT_ADOC"
+done
+
+# 計算仕様書の変換（公開用）
+for ADOC in $(find "$DIR" -name '*.adoc' -maxdepth 1 -printf '%f\n'); do
+    INPUT_ADOC="$ADOC"
+    OUTPUT_HTML=docs/dev/${ADOC/.adoc/.html}
+    echo "Convert '$INPUT_ADOC' to '$OUTPUT_HTML'."
+    asciidoctor -a for-dev -o "$DIR"/"$OUTPUT_HTML" "$DIR"/"$INPUT_ADOC"
 done
 
 # CSVからXMLへの変換仕様書の変換
